9741366a6a662c1cdf6865c1e0a145bda16eb927
Mononke17 <Mononke17@protonmail.com>
2024-03-08 19:03:54 +0100

    init 2

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-consdole.log("kek", "kek")
+const fs = require("node:fs");
+//(?<=\d+\s{2}).*?(?=\s<https:\/\/lolpros\.gg\/team|$|\s\s)
+
+function getplayernames(filePath:string) {
+
+}  
+
+
+function getplayerlinks(filepath:string) {
+  
+}
+
+function getplayerteams(filepath:string) {
+fs.readfileSync(filepath) 
+}
